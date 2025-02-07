adf19ed2dc4caec2fe53d71626020e471b47ca32
ayu <umaminokiami@gmail.com>
2025-02-07 21:54:11 +0900

    fix:code

--- server/api/[watchword]/codeExchange.ts
+++ server/api/[watchword]/codeExchange.ts
@@ -3,8 +3,29 @@ import { getCodeChange } from '~/utils/getCodeChange';
 import diffCode from '~/utils/diffCode';
 import { db } from '~/utils/db';
 
-const oldCode =
-  '#include <stdio.h>\nint main() {// メッセージを出力 \n printf("Hello, World!dgfdfgsdadgda\\n");\n return 0;\n}';
+const oldCode = `#include <stdio.h> 
+int my_sum(int* ,int); 
+int main(){ 
+	int ary[5] = {1,2,3,4,5};
+	int sum = 0;
+	int i;
+	sum = my_sum(ary,sizeof(ary) / sizeof(int));
+	for(i=0;i<5;i++){
+		printf("%d ",ary[i]);
+	}
+	printf("\\n");
+	printf("sum = %d\n",sum);	
+	return 0;
+}
+int my_sum(int* ary,int size){
+	int sum = 0;
+	int i;
+	
+	for(i=0;i<size;i++){
+		sum += *(ary + i);
+	}
+	return sum;
+}`;
 
 // 変更したコードを受け取って交換するエンドポイント(newCodeを受け取る)
 
