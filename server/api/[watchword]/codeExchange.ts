4b22c34159a047e693a509d61a07abb811a00439
ayu <umaminokiami@gmail.com>
2025-02-08 10:19:47 +0900

    fix:oldCode

--- server/api/[watchword]/codeExchange.ts
+++ server/api/[watchword]/codeExchange.ts
@@ -7,25 +7,24 @@ const oldCode = `
 #include <stdio.h> 
 int my_sum(int* ,int); 
 int main(){ 
-	int ary[5] = {1,2,3,4,5};
-	int sum = 0;
-	int i;
-	sum = my_sum(ary,sizeof(ary) / sizeof(int));
-	for(i=0;i<5;i++){
-		printf("%d ",ary[i]);
-	}
-	printf("\\n");
-	printf("sum = %d\\n",sum);	
-	return 0;
+int ary[5] = {1,2,3,4,5};
+int sum = 0;
+int i;
+sum = my_sum(ary,sizeof(ary) / sizeof(int));
+for(i=0;i<5;i++){
+printf("%d ",ary[i]);
+}
+printf("\\n");
+printf("sum = %d\\n",sum);	
+return 0;
 }
 int my_sum(int* ary,int size){
-	int sum = 0;
-	int i;
-	
-	for(i=0;i<size;i++){
-		sum += *(ary + i);
-	}
-	return sum;
+int sum = 0;
+int i;	
+for(i=0;i<size;i++){
+sum += *(ary + i);
+}
+return sum;
 }`;
 
 // 変更したコードを受け取って交換するエンドポイント(newCodeを受け取る)
