15c3e236bba91de7892d3e13ed5dce935e96b0a6
ayu <umaminokiami@gmail.com>
2025-02-08 10:58:36 +0900

    fix:diffCode

--- server/utils/diffCode.ts
+++ server/utils/diffCode.ts
@@ -1,22 +1,23 @@
 import { DiffMatchPatch } from 'diff-match-patch-ts';
 
-//コードの書き換えられたところを * //変更 * と追加する関数
-
 export default function diffCode(oldCode: string, newCode: string) {
   const dmp = new DiffMatchPatch();
   const diff = dmp.diff_main(oldCode, newCode);
   dmp.diff_cleanupSemantic(diff);
+
   let modifiedCode = '';
+
   for (let i = 0; i < diff.length; i++) {
     const [operation, text] = diff[i];
 
     if (operation === -1) {
-      // 削除された部分に `//変更` を挿入
-      modifiedCode += '//del';
+      // 削除された部分に `//del` を追加するが、削除内容は表示しない
+      modifiedCode += '/*del*/';
     } else {
       // 追加またはそのままの部分は維持
       modifiedCode += text;
     }
   }
+
   return modifiedCode;
 }
